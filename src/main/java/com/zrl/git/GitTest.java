669466f9f3bc760372a7a05dc51602387a264790
zrl <zrl@qq.com>
2022-01-24 15:10:51 +0800

    second commit

--- src/main/java/com/zrl/git/GitTest.java
+++ src/main/java/com/zrl/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zrl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
